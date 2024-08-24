c152168ad47296a29786572f802888f89e8583b4
gameoflord012 <manhhieu012@gmail.com>
2024-08-24 22:01:25 +0700

    Test Commit

--- pushgit.sh
+++ pushgit.sh
@@ -5,6 +5,11 @@ readarray -t backups < backup-files.txt;
 for file in $"${backups[@]}"
 do
     new_name=$(echo "$file" | tr / .)
+
+    if [[-z "$file"]]; then
+       echo "empty"
+    fi
+    
     echo "$new_name"
     cp "$file" "./$new_name"
 done
