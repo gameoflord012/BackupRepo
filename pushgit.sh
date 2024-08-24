4ef8d4d0d6dc4944188dba4da17853b3b8e0b461
gameoflord012 <manhhieu012@gmail.com>
2024-08-24 21:42:10 +0700

    Test Commit

--- pushgit.sh
+++ pushgit.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+readarray -t backups < backup-files.txt;
+
+for file in $"${backups[@]}"
+do
+    new_name=$(echo "$file" | tr / .)
+    echo "$new_name"
+    cp -i "$file" "./$new_name"
+done
+
 GIT=`which git`
 ${GIT} add --all .
 ${GIT} commit -m "Test Commit"
