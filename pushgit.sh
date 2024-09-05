8eda8964c027c09512e4f8fb3d3f3e8366eb2d3a
gameoflord012 <manhhieu012@gmail.com>
2024-09-05 15:14:08 +0700

    Test Commit

--- pushgit.sh
+++ pushgit.sh
@@ -6,14 +6,14 @@ readarray -t backups < backup-files.txt;
 
 for file in $"${backups[@]}"
 do
-    new_name=$(echo "$file" | tr / .)
+    new_name=./backup-files/$(echo "$file" | tr / .)
 
     if [[ -z "${file}" || "$file" =~ ^# ]]; then
 	continue
     fi
     
     echo newname: "$new_name"
-    cp "$file" "./$new_name"
+    cp "$file" "$new_name"
 done
 
 ${GIT} add --all .
